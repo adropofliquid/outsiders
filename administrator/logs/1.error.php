60f058aa81840711fa7a1fb9d94246f91ca56119
adropofliquid <74161050+adropofliquid@users.noreply.github.com>
2021-08-31 11:54:20 -0400

    Initial

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-07-18 15:00:43 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-07-18T15:00:43+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-08-02T07:35:09+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
+2021-08-03T14:22:12+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
+2021-08-12T10:35:50+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
+2021-08-13T11:51:52+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
